e6b82ea97a4cea59adf536307d10b23998b48a06
adinath <adinath@acciente.com>
2008-03-24 08:26:51 +0000

    days work (home office)

--- project/demoapp/trunk/src/demoapp/controller/HelloArizona.java
+++ project/demoapp/trunk/src/demoapp/controller/HelloArizona.java
@@ -1,22 +1,36 @@
 package demoapp.controller;
 
 import com.acciente.commons.javac.CompiledClass;
+import com.acciente.dragonfly.controller.Controller;
+import com.acciente.dragonfly.controller.Response;
 
-public class HelloArizona
+import java.io.IOException;
+
+public class HelloArizona implements Controller
 {
-	CompiledClass	oCompiledClass;
+	CompiledClass oCompiledClass;
 
 	public HelloArizona()
 	{
 		oCompiledClass = new CompiledClass( "HelloArizona.CompiledClass", null );
 
-		System.out.println( "Hello from HelloArizona(Phoenix) constructor" );
+      System.out.println( "oCompiledClass" + oCompiledClass );
+
+      System.out.println( "Hello from HelloArizona(Phoenix) constructor" );
       System.out.println( "Hello from HelloArizona(Scottsdale) constructor" );
       System.out.println( "Hello from HelloArizona(Kayenta) constructor" );
 	}
 
-	public void handler()
-	{
-		System.out.println( "Hello from handler() in HelloArizona" );
+	public void handler( Response oResponse ) throws IOException
+   {
+		oResponse.out().println( "Hello Generic! from handler() in HelloArizona" );
 	}
+
+   public void john( Response oResponse ) throws IOException
+   {
+      for ( int i = 0; i < 900; i++ )
+      {
+         oResponse.out().println( "<br>" + i + ": Hello John! this is the HelloArizona controller" );
+      }
+   }
 }
