d9e7c01ab2dc8e66ca8e97e40d77fbff02afad6b
adinath@acciente.com <adinath@acciente.com@0f1de9b8-5993-11de-b36e-3fc19fc75de0>
2008-04-01 17:36:45 +0000

    days work (home office)

--- project/dragonfly/trunk/src/com/acciente/dragonfly/controller/Form.java
+++ project/dragonfly/trunk/src/com/acciente/dragonfly/controller/Form.java
@@ -3,16 +3,25 @@ package com.acciente.dragonfly.controller;
 import com.acciente.dragonfly.dispatcher.form.File;
 
 /**
+ * This interface provides access to all parameters submitted as part of the request. Both GET and
+ * POST parameters are accesible. If the same parameter is defined via GET and POST the value sent
+ * via POST prevails.
+ *
  * Log
  * Feb 25, 2008 APR  -  created
  */
 public interface Form
 {
    String   getString( String sParamName );
+
    int      getInteger( String sParamName );
+
    float    getFloat( String sParamName );
+
    long     getLong( String sParamName );
+
    boolean  getBoolean( String sParamName );
+
    File     getFile( String sParamName );
 }
 
