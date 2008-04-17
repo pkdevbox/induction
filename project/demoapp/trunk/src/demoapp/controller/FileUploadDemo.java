6350c3f7a072209228bc1d967399318622fd9250
adinath@acciente.com <adinath@acciente.com@0f1de9b8-5993-11de-b36e-3fc19fc75de0>
2008-04-17 02:29:26 +0000

    - wrote file upload demo

---
+++ project/demoapp/trunk/src/demoapp/controller/FileUploadDemo.java
@@ -0,0 +1,67 @@
+package demoapp.controller;
+
+import com.acciente.dragonfly.controller.Response;
+import com.acciente.dragonfly.controller.Controller;
+import com.acciente.dragonfly.controller.Form;
+import com.acciente.commons.htmlform.ParserException;
+import com.acciente.commons.htmlform.FileHandle;
+
+import java.io.IOException;
+import java.io.File;
+
+import org.apache.commons.fileupload.FileUploadException;
+
+/**
+ * This class ...
+ *
+ * Log
+ * Apr 16, 2008 APR  -  created
+ */
+public class FileUploadDemo implements Controller
+{
+   public void handler( Response oResponse, Form oForm ) throws Exception, FileUploadException, ParserException
+   {
+      oResponse.setContentType( "text/plain" );
+      oResponse.out().println( "DragonFly - File Upload Test - Response" );
+      oResponse.out().println( "----------------------------------------" );
+
+      oResponse.out().println( "" );
+      oResponse.out().println( "" );
+
+      oResponse.out().println( "General info" );
+      oResponse.out().println( "------------" );
+      oResponse.out().println( "\tform parameters: " + oForm.getParamNames() );
+
+      oResponse.out().println( "" );
+      oResponse.out().println( "" );
+
+      oResponse.out().println( "About the string you entered" );
+      oResponse.out().println( "----------------------------" );
+      oResponse.out().println( "\tvalue: \"" + oForm.getString( "test_string" )+ "\"" );
+
+      FileHandle oUploadedFile = oForm.getFile( "test_file" );
+
+      File oDumpFile = null;
+      if ( oUploadedFile.getSize() > 0 )
+      {
+         oDumpFile = File.createTempFile( "test-", "-" + ( new File( oUploadedFile.getOriginFilename() ) ).getName(), new File( "c:/temp" ) );
+
+         oUploadedFile.write( oDumpFile );
+      }
+
+      oResponse.out().println( "" );
+      oResponse.out().println( "" );
+      oResponse.out().println( "About the file you uploaded" );
+      oResponse.out().println( "---------------------------" );
+      oResponse.out().println( "\tfile name   : " + oUploadedFile.getOriginFilename() );
+      oResponse.out().println( "\tcontent type: " + oUploadedFile.getContentType() );
+      oResponse.out().println( "\tsize        : " + oUploadedFile.getSize() );
+
+      if ( oDumpFile != null )
+      {
+         oResponse.out().println( "contents of uploaded file written to: " + oDumpFile.getCanonicalPath() );
+      }
+   }
+}
+
+// EOF
