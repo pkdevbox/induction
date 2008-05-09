6fe1f3ecebe5895a672444f128fbda2507a3be91
adinath <adinath@acciente.com>
2008-05-09 07:03:50 +0000

    - updated TODO entries in source

--- project/dragonfly/trunk/src/com/acciente/dragonfly/dispatcher/HttpDispatcher.java
+++ project/dragonfly/trunk/src/com/acciente/dragonfly/dispatcher/HttpDispatcher.java
@@ -165,6 +165,10 @@ public class HttpDispatcher extends HttpServlet
 
          if ( oControllerReturnValue != null )
          {
+            // todo: Problem : we need to handle the change detection/reload of the view classes somewhere!
+            // todo: Ideas   : if we can find via the classloader all the class a controller depends on (including
+            // todo:           the views) then we can reload these classes *before* the controller is invoked
+
             try
             {
                _oViewExecutor.execute( oControllerReturnValue, oResponse );
