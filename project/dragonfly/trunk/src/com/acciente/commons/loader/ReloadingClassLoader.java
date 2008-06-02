0a81a10b37139b659ea930fb7d691cb014f0c02d
adinath@acciente.com <adinath@acciente.com@0f1de9b8-5993-11de-b36e-3fc19fc75de0>
2008-06-02 21:05:13 +0000

    - this code released as beta-1.0.2 to customer - bug: changes to a view is not causing view class to reload (reported by customer)   - bug in JavaCompiledClassDef   - was able to reproduce bug   - fixed in this commit

--- project/dragonfly/trunk/src/com/acciente/commons/loader/ReloadingClassLoader.java
+++ project/dragonfly/trunk/src/com/acciente/commons/loader/ReloadingClassLoader.java
@@ -84,9 +84,11 @@ public class ReloadingClassLoader extends SecureClassLoader
          if ( oClassControlBlock.getClassDef().isModified() )
          {
             bReload = true;
+            //System.out.println( "reloading-classloader > reloading > class modified" );      // todo: remove
          }
          else
          {
+            //System.out.println( "reloading-classloader > checking dependent classes for: " + sClassName );      // todo: remove
             // before we can determine if we need to reload this class we need to
             // check and if needed reload the classes referenced by this class
             Class[]  aoReferencedClasses = oClassControlBlock.getReferencedClasses();
@@ -95,12 +97,14 @@ public class ReloadingClassLoader extends SecureClassLoader
             // we will proceed to reload this class
             for ( int i = 0; i < aoReferencedClasses.length; i++ )
             {
+               //System.out.println( "reloading-classloader > checking class: " + aoReferencedClasses[ i ] );      // todo: remove
                Class oCurrentReferencedClass = loadClass( aoReferencedClasses[ i ].getName() );
 
                if ( aoReferencedClasses[ i ] != oCurrentReferencedClass )
                {
                   aoReferencedClasses[ i ] = oCurrentReferencedClass;
                   bReload = true;
+                  //System.out.println( "reloading-classloader > reload caused by class: " + aoReferencedClasses[ i ] );      // todo: remove
                }
             }
          }
