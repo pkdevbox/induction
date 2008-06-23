70f4b39de1258295006896df1b75a8876325d0ea
adinath@acciente.com <adinath@acciente.com@0f1de9b8-5993-11de-b36e-3fc19fc75de0>
2008-06-23 20:32:21 +0000

    - added copyright/license header in all source files (*.java and *.xml) - created LICENSE and NOTICE files

--- project/induction/trunk/src/com/acciente/induction/dispatcher/ParamResolver.java
+++ project/induction/trunk/src/com/acciente/induction/dispatcher/ParamResolver.java
@@ -1,3 +1,20 @@
+/**
+ *   Copyright 2008 Acciente, LLC
+ *
+ *   Acciente, LLC licenses this file to you under the
+ *   Apache License, Version 2.0 (the "License"); you
+ *   may not use this file except in compliance with the
+ *   License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in
+ *   writing, software distributed under the License is
+ *   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ *   OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
 package com.acciente.induction.dispatcher;
 
 import com.acciente.induction.controller.Form;
@@ -33,7 +50,7 @@ public class ParamResolver
       _oFileUploadConfig   = oFileUploadConfig;
    }
 
-   public Object getParameterValue( Class oParamClass, HttpServletRequest oRequest, HttpServletResponse oResponse ) 
+   public Object getParameterValue( Class oParamClass, HttpServletRequest oRequest, HttpServletResponse oResponse )
       throws ParamResolverException, MethodNotFoundException, ClassNotFoundException, InvocationTargetException, ParameterProviderException, ConstructorNotFoundException, InstantiationException, IllegalAccessException
    {
       Object   oParamValue = null;
