13aa265cecd76d0fce5cd71fed381bd2d823c8e7
adinath <adinath@acciente.com>
2008-06-23 08:18:57 +0000

    - moved content drafts to inductionframework.org project - improved make_latest_release.bat to work on laptop and work environments

--- project/induction/trunk/script/make_latest_release.bat
+++ project/induction/trunk/script/make_latest_release.bat
@@ -2,11 +2,32 @@
 @rem Log
 @rem May 27, 2008 APR	created
 
-@startlocal
+@rem startlocal
 @set path=%path%;c:\dev\j2sdk1.4.2_16\bin
-@set release_root=\acciente-projects\software\release\induction\latest
-@set compiled_class_root=\acciente-projects\software\project\induction\class\prod
-@set demoapp_root=\acciente-projects\software\project\demoapp
+
+@if exist c:\acciente\acciente-projects\software  goto :home_root
+@if exist t:\acciente-projects\software           goto :work_root
+@goto :error_root
+
+@:home_root
+@set common_root=c:\acciente\acciente-projects\software
+@goto :end_root
+
+@:work_root
+@set common_root=t:\acciente-projects\software
+@goto :end_root
+
+@:error_root
+@echo none of exected root path(s) found
+@goto :end_script
+
+@:end_root
+
+@echo using root %common_root%
+@set release_root=%common_root%\release\induction\latest
+@set compiled_class_root=%common_root%\project\induction\class
+@set sample_conf_file=%common_root%\project\induction\subversion\conf\induction-complete-sample-config.xml
+@set demoapp_root=%common_root%\project\demoapp
 
 @rd %release_root% /s/q
 @md %release_root%
@@ -14,7 +35,11 @@
 @jar -cfM %release_root%\acciente-induction-beta-latest.jar	-C %compiled_class_root% /com/acciente/induction
 @jar -cfM %release_root%\acciente-commons-beta-latest.jar	-C %compiled_class_root% /com/acciente/commons
 @jar -cfM %release_root%\demoapp-src.jar			-C %demoapp_root%        /
+@copy %sample_conf_file%                                           %release_root%	
 
-@echo Release package to: %release_root%
+@echo Released package to: %release_root%
 
-@endlocal
+:end_script
+
+@rem endlocal
+@pause
