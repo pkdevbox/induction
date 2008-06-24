ed7b56b2abbcac883186d5b781ec3c0b335dd567
adinath <adinath@acciente.com>
2008-06-24 08:19:36 +0000

    - improved release scripts - removed references to source class loader in induction-complete-sample-config.xml

--- project/induction/trunk/scripts/make-latest-release.bat
+++ project/induction/trunk/scripts/make-latest-release.bat
@@ -4,6 +4,9 @@
 
 @set path=%path%;c:\dev\jdk1.6.0_04\bin
 
+@if %1_==_ goto :error_usage
+@set release_version=%1
+
 @if exist c:\acciente\acciente-projects\software  goto :home_root
 @if exist t:\acciente-projects\software           goto :work_root
 @goto :error_root
@@ -16,28 +19,59 @@
 @set common_root=t:\acciente-projects\software
 @goto :end_root
 
-@:error_root
-@echo none of exected root path(s) found
-@goto :end_script
-
 @:end_root
 
-@echo using root %common_root%
-@set release_root=%common_root%\release\induction\latest
+@echo INFO: using root %common_root%
+@set release_root=%common_root%\release\induction\%release_version%
 @set compiled_class_root=%common_root%\project\induction\class
+@set source_class_root=%common_root%\project\induction\subversion\src
+@set javadoc_root=%common_root%\project\induction\javadoc
 @set sample_conf_file=%common_root%\project\induction\subversion\conf\induction-complete-sample-config.xml
 @set demoapp_root=%common_root%\project\demoapp
+
+@if exist %release_root% goto :error_version_exists
 
 @rd %release_root% /s/q
 @md %release_root%
 
-@jar -cfM %release_root%\acciente-induction-beta-latest.jar	-C %compiled_class_root% /com/acciente/induction
-@jar -cfM %release_root%\acciente-commons-beta-latest.jar	-C %compiled_class_root% /com/acciente/commons
-@jar -cfM %release_root%\demoapp-src.jar			-C %demoapp_root%        /
-@copy %sample_conf_file%                                           %release_root%	
+@rem -- make a temp copy of the source to remove .svn files
+@xcopy %source_class_root% %release_root%\tmp-src /s /i /exclude:src-excludes.txt
 
-@echo Released package to: %release_root%
+@rem -- make a release of Acciente Commons
+@jar -cfM %release_root%\acciente-commons-%release_version%.jar			-C %compiled_class_root%	/com/acciente/commons
+@jar -cfM %release_root%\acciente-commons-%release_version%-sources.jar		-C %release_root%\tmp-src	/com/acciente/commons
+@jar -cfM %release_root%\acciente-commons-%release_version%-javadoc.jar		-C %javadoc_root%/commons	/
 
-:end_script
+@rem -- make a release of Acciente Induction
+@jar -cfM %release_root%\acciente-induction-%release_version%.jar		-C %compiled_class_root%	/com/acciente/induction
+@jar -cfM %release_root%\acciente-induction-%release_version%-sources.jar	-C %release_root%\tmp-src	/com/acciente/induction
+@jar -cfM %release_root%\acciente-induction-%release_version%-javadoc.jar	-C %javadoc_root%/induction	/
+@copy %sample_conf_file%							%release_root%
+
+@rem -- remove temp source directory
+@rd %release_root%\tmp-src /s/q
+
+@rem -- make a release of Acciente DemoApp
+@rem jar -cfM %release_root%\demoapp-src.jar					-C %demoapp_root%        	/ 
+
+@echo INFO: released package to: %release_root%
+@goto :end_script
+
+@:error_usage
+@echo INFO: Usage:
+@echo INFO: make-release-latest version-number-or-name
+@echo INFO: e.g: make-release-latest beta-1.1.0
+@echo .
+@goto end_script
+
+@:error_version_exists
+@echo ERROR: there is already a version named %release_version%
+@goto end_script
+
+@:error_root
+@echo ERROR: none of exected root path(s) found
+@goto :end_script
+
+@:end_script
 
 @pause
