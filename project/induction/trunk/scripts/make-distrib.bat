4f085af02d49d44309a768073cc8492f7216e2f9
adinath@acciente.com <adinath@acciente.com@0f1de9b8-5993-11de-b36e-3fc19fc75de0>
2008-07-31 16:20:05 +0000

    - script changes

--- project/induction/trunk/scripts/make-distrib.bat
+++ project/induction/trunk/scripts/make-distrib.bat
@@ -46,7 +46,7 @@
 @set induction_distrib_core_content=%induction_distrib_core_content% %release_root%\*.txt 
 @set induction_distrib_core_content=%induction_distrib_core_content% %release_root%\*.xml 
 @7z a -tzip    %induction_distrib_core_file% %induction_distrib_core_content%
-@7z a -tzip -r %induction_distrib_core_file% %release_root%\*.jar
+@7z u -tzip -r %induction_distrib_core_file% %release_root%\*.jar
 
 @rem -- create the distribution for the supporting libs
 @set induction_distrib_req_libs_content=
