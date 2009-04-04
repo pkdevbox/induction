16572445b266f7c05da6913bd2d9e557f51f524f
adinath <adinath@acciente.com>
2009-04-04 04:12:04 +0000

    - new demo controller to show class loaders - moved Cookie controller to its own package

--- project/demoapp/trunk/src/demoapp/counter_app/ShowController.java
+++ project/demoapp/trunk/src/demoapp/counter_app/ShowController.java
@@ -18,39 +18,20 @@
 package demoapp.counter_app;
 
 import com.acciente.induction.controller.Controller;
-import com.acciente.induction.controller.Request;
 import com.acciente.induction.controller.Response;
 
-import javax.servlet.http.Cookie;
 import java.io.IOException;
 
 /**
  * Log
  * Jun 8, 2008 APR  -  created
  */
-public class Cookies implements Controller
+public class ShowController implements Controller
 {
-   public void handler( Request oRequest, Response oResponse )
+   public void handler( Response oResponse, Counter oCounter )
       throws IOException
    {
-      if ( oRequest.getCookies() == null )
-      {
-         oResponse.out().println( "The request has no cookies!" );
-      }
-      else
-      {
-         for ( int i = 0; i < oRequest.getCookies().length; i++ )
-         {
-            Cookie oCookie = oRequest.getCookies()[ i ];
-
-            oResponse.out().println( "Name=" + oCookie.getName() );
-            oResponse.out().println( "Domain=" + oCookie.getDomain() );
-            oResponse.out().println( "Path=" + oCookie.getPath() );
-            oResponse.out().println( "Secure=" + oCookie.getSecure() );
-            oResponse.out().println( "Value=" + oCookie.getValue() );
-            oResponse.out().println( "Version=" + oCookie.getVersion() );
-         }
-      }
+      oResponse.out().println( "count is: " + oCounter.getCount() );
    }
 }
 
