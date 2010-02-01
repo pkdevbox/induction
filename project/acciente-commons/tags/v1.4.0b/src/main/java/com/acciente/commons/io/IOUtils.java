61d77154fc225ad6190d171de3312a472cdf2a7a
adinath@acciente.com <adinath@acciente.com@0f1de9b8-5993-11de-b36e-3fc19fc75de0>
2010-02-01 03:14:42 +0000

    - source snapshot of release 1.4.0b

---
+++ project/acciente-commons/tags/v1.4.0b/src/main/java/com/acciente/commons/io/IOUtils.java
@@ -0,0 +1,81 @@
+/**
+ *   Copyright 2009 Acciente, LLC
+ *
+ *   Acciente, LLC licenses this file to you under the
+ *   Apache License, Version 2.0 (the "License"); you
+ *   may not use this file except in compliance with the
+ *   License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in
+ *   writing, software distributed under the License is
+ *   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ *   OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+package com.acciente.commons.io;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * A collection of general purpose I/O utils.
+ *
+ * @created Feb 20, 2008
+ * @author APR
+ */
+public class IOUtils
+{
+   public static int   FILEIO_BUFFER_SIZE = 512;
+
+   public static void copy( File oFrom, OutputStream oTo ) throws IOException
+   {
+      FileInputStream oFromStream = new FileInputStream( oFrom );
+
+      try
+      {
+         byte[]   ayBuffer = new byte[ FILEIO_BUFFER_SIZE ];
+         int      iBytesRead;
+         while ( ( iBytesRead = oFromStream.read( ayBuffer ) ) == FILEIO_BUFFER_SIZE )
+         {
+            oTo.write( ayBuffer );
+         }
+         oTo.write( ayBuffer, 0, iBytesRead );
+         oTo.flush();
+      }
+      finally
+      {
+         oFromStream.close();
+      }
+   }
+
+   public static void copy( Reader oFrom, File oTo ) throws IOException
+   {
+      Writer   oToWriter = new FileWriter( oTo );
+
+      try
+      {
+         char[]   acBuffer = new char[ FILEIO_BUFFER_SIZE ];
+         int      iCharsRead;
+         while ( ( iCharsRead = oFrom.read( acBuffer ) ) == FILEIO_BUFFER_SIZE )
+         {
+            oToWriter.write( acBuffer );
+         }
+         oToWriter.write( acBuffer, 0, iCharsRead );
+         oToWriter.flush();
+      }
+      finally
+      {
+         oToWriter.close();
+      }
+   }
+}
+
+// EOF
---
+++ project/acciente-commons/tags/v1.4.0b/src/main/java/com/acciente/commons/loader/ResourceDef.java
@@ -0,0 +1,57 @@
+/**
+ *   Copyright 2009 Acciente, LLC
+ *
+ *   Acciente, LLC licenses this file to you under the
+ *   Apache License, Version 2.0 (the "License"); you
+ *   may not use this file except in compliance with the
+ *   License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in
+ *   writing, software distributed under the License is
+ *   distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ *   OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+package com.acciente.commons.loader;
+
+/**
+ * This interface abstracts access to resource to be accessed via the classloader
+ *
+ * @created Sep 30, 2008
+ *
+ * @author Adinath Raveendra Raj
+ */
+public interface ResourceDef
+{
+   /**
+    * Returns a fully qualified class name of the that this object manages
+    *
+    * @return a string class name
+    */
+   String      getResourceName();
+
+   /**
+    * Returns true if the underlying source of the class data has changed since the
+    * last call to getData()
+    *
+    * @return true if changed, false otherwise
+    */
+   boolean     isModified();
+
+   /**
+    * Reloads the class byte code from the underlying source
+    */
+   void        reload();
+
+   /**
+    * This method loads and returns the contents of this resource
+    *
+    * @return the contents of the resource as a byte array
+    */
+   byte[]      getContent();
+}
+
+// EOF
